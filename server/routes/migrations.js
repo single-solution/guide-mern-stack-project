8c4326554c806c5cef74385ce45f37b11f373336
Muhammad Faiq Nadeem <chfaiqnadeem@gmail.com>
2025-06-12 19:17:36 +0500

    Delete server directory

--- server/routes/migrations.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-
-const { installSampleDB } = require("../controllers/migrations");
-
-const router = express.Router();
-
-router.get("/install-all", installSampleDB);
-
-module.exports = router;
--- server/utils/crons/scripts/verifyAndUpdateUserPaymentStatus.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-# Navigate to the server directory
-cd /home/ubuntu/workspace/server
-
-# Load environment variables from .env file
-source .env
-
-# Execute the Node.js script
-node utils/crons/verifyAndUpdateUserPaymentStatus.js
